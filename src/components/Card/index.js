fa18b68b7e90cda256a016835cbafd9d16e2928c
rdfenix <rhoodsk8erboy@gmail.com>
2019-10-25 10:54:57 -0300

    style added

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Container, Label } from './styles';
 
-const Card = () => {
-  return <div></div>;
+const Card = ({ data }) => {
+  return (
+    <Container>
+      <header>
+        {data.labels.map(label => (
+          <Label key={label} color={label} />
+        ))}
+      </header>
+      <p>{data.content}</p>
+      {data.user && <img src={data.user} alt='' />}
+    </Container>
+  );
 };
 
 export default Card;
